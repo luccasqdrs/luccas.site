5cd63f0bed308298d70058c560c004c036445bb0
Luccas Quadros <luccas.quadros@softplan.com.br>
2020-07-09 16:46:07 -0300

    INITIAL COMMIT

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Luccas Quadros Website (Under Construction)`,
+    description: `Luccas Quadros Personal Website, still under construction, just a placeholder for now. Developed with Gatsby`,
+    author: `@luccasqdrs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `luccas.site`,
+        short_name: `luccas.site`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
